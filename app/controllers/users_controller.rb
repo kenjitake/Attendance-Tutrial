c957928d477e620b8b07f024fa39ac397930610c
kenji <kentake@email.com>
2022-12-23 08:19:45 +0000

    5.5.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user=User.find(params[:id])
+  end
+  
   def new
+    @user=User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+  end
+  
+  def create
+    @user=User.new(params[:user])
+    
+    if @user.save
+      
+    else 
+      render:new
+      
+    end
+  end
+  
+  
+  pravate
+  
+  def user_params
+   params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 end
