997e54ba4db2c86b04ea27f8bb25ba07d4ff2b76
kenji <kentake@email.com>
2022-12-24 06:04:46 +0000

    5.5.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user=User.new(params[:user])
+    @user=User.new(user_params)
     
     if @user.save
       
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
   
   
-  pravate
+  private
   
   def user_params
    params.require(:user).permit(:name,:email,:password,:password_confirmation)
