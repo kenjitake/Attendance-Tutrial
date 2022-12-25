fc6ead126130a803613f1aacfa4807b76c106fc6
kenji <kentake@email.com>
2022-12-25 06:42:25 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     @user=User.new(user_params)
     
     if @user.save
+      flash[:success]='新規作成に成功しました。'
+      redirect_to @user
       
     else 
       render:new
