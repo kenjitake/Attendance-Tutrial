a91ec305af49aeed4b86ef39637f7387b24841a9
kenji <kentake@email.com>
2023-01-04 05:15:25 +0000

    6-6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
   
   def create
     
-    user=User.find_by(email:params[:sessions][:email].downcase)
-    if user && user.authenticate(params[:sessions][:password])
-    
+   user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
+      flash.now[:danger]='認証に失敗しました。'
      render:new
      
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
-  
-  get '/login' ,to: 'sessions#new'
-  post '/login' ,to: 'sessions#create'
-  delete '/logout' ,to: 'sessions#destroy'
-  
-  resources:users
+
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users
 end
